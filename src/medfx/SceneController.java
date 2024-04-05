4aff91fe7672cdee5a5470de2f0438efd18ddd88
jpache22 <julissa.school101@gmail.com>
2024-04-05 12:12:01 -0700

    Added SceneControllerClass to MedFX
    Added SceneController class and added 3 methods for the three screens currently in the repo

---
+++ src/medfx/SceneController.java
@@ -0,0 +1,71 @@
+package medfx;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class SceneController 
+{	
+	private static final int LENGTH = 800;
+	private static final int WIDTH = 500;
+	
+	/**
+	 * Switches the current page to the CreateAccountPage
+	 * This is a STATIC method, all the methods in this class will be static
+	 * This means the way you would use this method in your code would be SceneController.switchToCreateAccountPage(e)
+	 * We do not need to make a SceneController object.
+	 * 
+	 * @param e ActionEvent which triggers screen switch
+	 * @throws IOException
+	 */
+	public static void switchToCreateAccountPage(ActionEvent e) throws IOException // needs to throw IOException because of the image in create account page, if you don't use the image might not be needed
+	{
+		Parent root = new CreateAccountPage(); // Parent is the parent class for any screen in JavaFX
+		Stage stage = (Stage) ((Node)e.getSource()).getScene().getWindow(); //gets the window where the button is being pressed using the ActionEvent (like a button press or something)
+		Scene scene = new Scene(root, LENGTH, WIDTH); //creates a new scene
+		stage.setScene(scene); //changes the stage 
+		stage.show(); //shows new stage
+	}
+	
+	/**
+	 * Switches the current page to the UserMainPage
+	 * @param e ActionEvent which triggers screen switch
+	 * @throws IOException
+	 */
+	public static void switchToUserMainPage(ActionEvent e) throws IOException // needs to throw IOException because of the image in create account page, if you don't use the image might not be needed
+	{
+		Parent root = new UserMainPage(); // Parent is the parent class for any screen in JavaFX
+		Stage stage = (Stage) ((Node)e.getSource()).getScene().getWindow(); //gets the window where the button is being pressed using the ActionEvent (like a button press or something)
+		Scene scene = new Scene(root, LENGTH, WIDTH); //creates a new scene
+		stage.setScene(scene); //changes the stage 
+		stage.show(); //shows new stage
+	}
+	
+	/**
+	 * Switches the current page to the PatientViewPage
+	 * NOTE: MAY NEED TO REMOVE THROWS IOEXCEPTION IF IMAGE IS REMOVED
+	 * @param e ActionEvent which triggers screen switch
+	 * @throws IOException
+	 */
+	public static void switchToPatientView(ActionEvent e) throws IOException // needs to throw IOException because of the image in create account page, if you don't use the image might not be needed
+	{
+		Parent root = new PatientViewPage(); // Parent is the parent class for any screen in JavaFX
+		Stage stage = (Stage) ((Node)e.getSource()).getScene().getWindow(); //gets the window where the button is being pressed using the ActionEvent (like a button press or something)
+		Scene scene = new Scene(root, LENGTH, WIDTH); //creates a new scene
+		stage.setScene(scene); //changes the stage 
+		stage.show(); //shows new stage
+	}
+	
+	/**
+	 * To create your own method to switch screens just copy and past the method above, change the name,
+	 * and then change the Parent root = new CreateAccountPage(); line to instantiate whatever page you want to switch to
+	 * Make sure to remove the throws clause if your constructor for your page doesn't have to handle exceptions like mine
+	 */
+	
+	
+}
+
